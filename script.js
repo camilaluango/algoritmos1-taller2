fba16a30391a019a35c850395098f7f01dbebbff
camila <mariacamilaluango@gmail.com>
2024-10-25 22:29:04 -0500

    ejercicio 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let hora = parseInt (prompt("que hora es? formato de 24h"))
 
-if (hora>0 && hora<6){
+if (hora>=0 && hora<6){
 document.write("buena madrugada")
 }
 else if(hora>=6 && hora<=12){
@@ -12,3 +12,7 @@ else if(hora>12 && hora<=19){
 else if(hora>19 && hora<=23){
     document.write("buenas noches")
 }
+
+let altura = parseFloat (prompt("cual es su altura? "))
+let peso =parseFloat (prompt("cual es su peso? "))
+let imc = peso/(altura*altura)
