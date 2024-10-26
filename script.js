2f0f0b4c9e852c98f5639c2debe1ef91b4f18865
camila <mariacamilaluango@gmail.com>
2024-10-25 22:18:26 -0500

    ejercicio 1 dias

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let hora = parseInt (prompt("que hora es? formato de 24h"))
 
-if (hora>=0 && hora<=5){
+if (hora>=0 && hora<6){
 document.write("buena madrugada")
+}
+else if(hora>=6 && hora<=12){
+document.write("buenos dias")
 }
