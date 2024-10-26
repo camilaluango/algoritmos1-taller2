f562c29048bc9ee2b9958ed48b429333d9c8fe28
camila <mariacamilaluango@gmail.com>
2024-10-25 23:52:50 -0500

    ejercicio 3 fin

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-/*let hora = parseInt (prompt("que hora es? formato de 24h"))
+document.write("ejercicio 1","<br>")
+let hora = parseInt (prompt("que hora es? formato de 24h"))
 
 if (hora>=0 && hora<6){
-document.write("buena madrugada")
+document.write("buena madrugada ")
 }
 else if(hora>=6 && hora<=12){
-document.write("buenos dias")
+document.write("buenos dias ")
 }
 else if(hora>12 && hora<=19){
-    document.write("buenas tardes")
+    document.write("buenas tardes ")
 }
 else if(hora>19 && hora<=23){
-    document.write("buenas noches")
+    document.write("buenas noches ")
 }
-
+document.write("<br>","ejercicio 2 ","<br>")
 let altura = parseFloat (prompt("cual es su altura? "))
 let peso =parseFloat (prompt("cual es su peso? "))
 let imc = peso/(altura*altura)
@@ -28,9 +29,9 @@ if(imc<18.5){
         }
             if(imc>30.0){
             document.write("usted esta en el rango de obesidad")
-            }*/
+            }
 
-
+document.write("<br>","ejercicio 3","<br>")
 let numO =parseInt((Math.random()*100)+1);
 console.log(numO)
 alert("adivina el numero")
@@ -40,17 +41,17 @@ while(opor>0){
     alert(`tienes ${opor} oportunidades`)
     let numA = parseInt(prompt("ingresa un numero"))
     if (numA==numO){
-        document.write("ganaste")
+        document.write("!!Ganaste!!")
         break
     }
-    else if (numA>numO){
+        else if (numA>numO){
         alert("el numero es mayor al numero secreto")
-    }
-    else if (numA<numO){
-        alert("el numero es menor al numero secreto")
-    }
+         }
+            else if (numA<numO){
+            alert("el numero es menor al numero secreto")
+            }
     opor--
 }
 if (opor==0){
-    document.write("perdiste")
+    document.write("Perdiste")
 }
